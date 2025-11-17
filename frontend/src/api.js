de46be8a3e19de421ac770a189191eda99de48ba
subhalakshmi-3009 <subha@alphaworkz.com>
2025-11-17 10:25:22 +0000

    TEMP: use ngrok backend for testing

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://192.168.0.103:5000';
+const API_BASE_URL = 'https://shera-undefensible-pseudoindependently.ngrok-free.dev';
 
 
 const api = axios.create({
